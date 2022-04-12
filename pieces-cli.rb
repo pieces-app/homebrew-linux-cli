bab7a11b95bedaeea1e02e1bb642820354e2b82f
Connor-OLeary-Pieces <connor@pieces.app>
2022-04-12 16:29:13 -0400

    v0.0.5

--- pieces-cli.rb
+++ pieces-cli.rb
@@ -1,13 +1,19 @@
-class PiecesCli < Formula
+class Pieces < Formula
   desc "A command line application for Pieces"
   homepage "https://code.pieces.app/"
-  url "https://storage.googleapis.com/pieces-cli-production-releases/pieces-cli-0.0.2.tar.gz"
-  sha256 "85ad00287431d08f37ba98f04ebac93777ae9f6bb1586fe197747bdb6ee66d40"
-  license "MIT"
+  url "https://storage.googleapis.com/pieces-cli-production-releases/pieces-cli-0.0.5.tar.gz"
+  sha256 "1627de9c4f3c9c5649c4a75ff3215dc234c4bdf92d8c27cc72f978416ca73416"
+  license ""
+
+  depends_on "xsel" => :recommended
 
   def install
+	if OS.linux?
 		bin.install "pieces-linux" => "pieces"
-		prefix.install "README.md"
-  end
+	else
+		bin.install "pieces-mac" => "pieces"
+	end
 
+	prefix.install "README.md"
+  end
 end
