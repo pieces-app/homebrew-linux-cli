1e3fd234b2a159035430fef339f9dd5fc1dcbacf
Connor-OLeary-Pieces <connor@pieces.app>
2022-04-14 16:03:09 -0400

    url update link

--- pieces.rb
+++ pieces.rb
@@ -1,7 +1,7 @@
 class Pieces < Formula
   desc "A command line application for Pieces"
   homepage "https://code.pieces.app/"
-  url "https://storage.googleapis.com/pieces-cli-production-releases/pieces-0.0.10.tar.gz"
+  url "https://storage.googleapis.com/app-releases-59612ba/pieces-cli/release/pieces-0.0.10.tar.gz"
   sha256 "1f731e9077c9f1e9dc8e331da64921623e1f5b058339ed6dfd9ef99d9e13c10e"
   license ""
 
