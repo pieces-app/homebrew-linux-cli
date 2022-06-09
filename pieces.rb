265110330477577577028ecac70526815ebbfc4b
Connor-OLeary-Pieces <connor@pieces.app>
2022-06-09 10:29:09 -0400

    version 1.1.0

--- pieces.rb
+++ pieces.rb
@@ -1,8 +1,8 @@
 class Pieces < Formula
   desc "A command line application for Pieces"
   homepage "https://code.pieces.app/"
-  url "https://storage.googleapis.com/app-releases-59612ba/pieces-cli/release/pieces-1.0.2.tar.gz"
-  sha256 "2a5245a0eca24dbd7d2f8c4d699ff93364eee4f2b94e7c4250e1c2581a84429e"
+  url "https://storage.googleapis.com/app-releases-59612ba/pieces-cli/release/pieces-1.1.0.tar.gz"
+  sha256 "9e143f95c22be62b4c462de0827e5011e31f5a9da520b1c47157ed6c86efce50"
   license ""
 
   on_linux do
